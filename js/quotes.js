fef903a9c0f634348e00dcc67cab11b4e44ec14a
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-27 15:15:34 -0400

    Wayne Gretzky Quotes

---
+++ js/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    source: 'Wayne Gretzky',
+    citation: "Hockey News, 'Bob McKenzie's Interview with Wayne Gretzky'",
+    year: 1983
+  },
+  {
+    quote: "'You miss 100% of the shots you don't take.' -Wayne Gretzky",
+    source: 'Michael Scott',
+    citation: "The Office, Season 5, Episode 23: 'Michael Scott Paper Company'",
+    year: 2009
+  }
+]
