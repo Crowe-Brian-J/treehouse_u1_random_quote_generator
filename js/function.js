0bb7f795379424e2501eb928fa1a63e27ede708b
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-27 15:52:24 -0400

    basic css taken from previous projects

--- js/function.js
+++ js/function.js
@@ -2,11 +2,17 @@ const quoteGen = () => {
   //stub for import quotes
   let quote = 'This is a quote.'
   let source = 'Brian Crowe'
-  //let citation
-  //let year = 2025
+  let citation = ''
+  let year = 2025
+
+  if (citation !== '') {
+    citation = `<div>${year} (from: ${citation})<div>`
+  } else {
+    citation = `<div>${year}</div>`
+  }
 
   //string for display on page
-  const strQuote = `<h1>${quote}</h1> <h3>-${source}</h3>`
+  const strQuote = `<div class="card"><h1>${quote}</h1> <h3>-${source}</h3> ${citation}</div> <button type="button">Get another quote!</button>`
 
   return strQuote
 }
